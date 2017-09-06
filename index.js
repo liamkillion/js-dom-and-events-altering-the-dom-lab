93e44247ee3abefcf0d724557a16696751a1680f
Jeff Katz <jeff@flatironschool.com>
2017-09-06 11:54:37 -0400

    updating index.js

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-function removeDowntown(){
-  document.querySelector('ul li').remove()
-}
 
-function hideCompanyStrategy(){
-  document.querySelector('#company-strategy').style.visibility = 'hidden'
-}
-
-function showCompanyStrategy(){
-  document.querySelector('#company-strategy').style.visibility = 'visible'
-}
-
-function changeTitle(title){
-  document.querySelector('h1').innerHTML = title
-}
