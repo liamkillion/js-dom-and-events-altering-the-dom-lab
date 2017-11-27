2335a9ba6488304c780c21306c849d2081343480
Liam Killion <liam.killion@gmail.com>
2017-11-27 10:37:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+function removeDowntown(){
+  document.querySelector('li').remove()
+}
 
+function hideCompanyStrategy(){
+  let strat = document.getElementById('company-strategy')
+  strat.style.visibility = "hidden"
+}
+
+function showCompanyStrategy(){
+  let strat = document.getElementById('company-strategy')
+  strat.style.visibility = "visible"
+}
+
+function changeTitle(title){
+  document.querySelector('h1').innerText = title
+}
